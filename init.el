2f83b86409f91bdd636e4829864a4b4fa37f8f85
David Yeung <quxbaz@gmail.com>
2020-10-16 16:18:44 -0600

    Update.

--- init.el
+++ init.el
@@ -61,7 +61,7 @@
 (require 'yasnippet)
 (yas-initialize)
 (yas-load-directory "~/.emacs.d/snippets")
-(setq yas-indent-line "fixed")
+;; (setq yas-indent-line "fixed")
 
 (add-to-list 'load-path "~/.emacs.d/el-get/wrap-region")
 (require 'wrap-region)
