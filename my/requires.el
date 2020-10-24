371e10206c80c3664d917d8cdebdc61f3b047c00
David Yeung <quxbaz@gmail.com>
2020-10-24 17:05:34 -0600

    + snippets/

--- my/requires.el
+++
@@ -1,8 +0,0 @@
-;; (add-to-list 'load-path "~/.emacs.d/yasnippet")
-;; (require 'yasnippet)
-;; (yas-initialize)
-
-; (yas-load-directory "~/.emacs.d/snippets")
-
-;; (add-to-list 'load-path "~/.emacs.d/el-get/wrap-region")
-;; (require 'wrap-region)
