9905895e6835ed61baf0496934d9d964cbe61600
David Yeung <quxbaz@gmail.com>
2020-10-25 21:59:29 -0600

    Bindings.

--- my/bindings.el
+++ my/bindings.el
@@ -4,7 +4,8 @@
 ;; Text navigation, selection
 (global-set-key "\C-\\" 'my-match-paren)
 ;; (global-set-key (kbd "M-.") 'my-mark-current-word)
-(global-set-key (kbd "S-SPC") 'my-swap-points)
+(global-set-key (kbd "C-<tab>") 'my-swap-points)
+;; (global-set-key (kbd "C-<backspace>") 'my-swap-points)
 
 
 ;; Editing
