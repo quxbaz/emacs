56721c2a25d6c499ab0dcf600fd8d388c8e695b4
David Yeung <quxbaz@gmail.com>
2021-12-01 14:01:27 -0700

    Move "kt" chord to hooks.

--- my/hooks.el
+++ my/hooks.el
@@ -30,6 +30,7 @@
             (electric-pair-mode 0)
             (highlight-indent-guides-mode t)
             (git-gutter-mode t)
+            (key-chord-define-local "kt" 'my-transpose-lines)
             (key-chord-define-local "qq" 'my-indent-block)
             (key-chord-define-local "zz" 'zap-up-to-char)))
 
