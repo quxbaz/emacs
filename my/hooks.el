202ccb1e224aef3af44f5faf1d094571c2f57378
David Yeung <quxbaz@gmail.com>
2022-03-20 23:02:58 -0600

    Map dired-mode F to my-find-dired.

--- my/hooks.el
+++ my/hooks.el
@@ -17,7 +17,7 @@
             (local-set-key (kbd "C-c C-c") 'dired-toggle-read-only)
             (local-set-key (kbd "C-+") 'dired-create-empty-file)
             (local-set-key (kbd "<return>") 'dired-find-alternate-file)
-            (local-set-key (kbd "F") 'find-name-dired)
+            (local-set-key (kbd "F") 'my-find-dired)
             (local-set-key (kbd "I") 'dired-do-isearch-regexp)
             (local-set-key (kbd "M-r") 'dired-do-query-replace-regexp)))
 
