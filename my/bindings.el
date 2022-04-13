03a6f2e5f2fc50e5880cf73621c56f86045687a0
David Yeung <quxbaz@gmail.com>
2022-04-13 14:50:17 -0600

    Add jump-to-register bindings (M-3, M-4).

--- my/bindings.el
+++ my/bindings.el
@@ -77,6 +77,8 @@
 (global-set-key (kbd "M-0") 'delete-window)
 (global-set-key (kbd "M-1") (lambda () (interactive) (jump-to-register (string-to-char "1"))))
 (global-set-key (kbd "M-2") (lambda () (interactive) (jump-to-register (string-to-char "2"))))
+(global-set-key (kbd "M-3") (lambda () (interactive) (jump-to-register (string-to-char "3"))))
+(global-set-key (kbd "M-4") (lambda () (interactive) (jump-to-register (string-to-char "4"))))
 (global-set-key (kbd "s-x") 'window-swap-states)
 
 
