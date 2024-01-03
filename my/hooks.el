f729c15ebe0e082c2e9c09b1282775bf15eeb240
David Yeung <quxbaz@gmail.com>
2024-01-03 15:50:15 -0700

    modified:   my/hooks.el   Enable git-gutter in org-mode.

--- my/hooks.el
+++ my/hooks.el
@@ -85,6 +85,7 @@
 
 (add-hook 'org-mode-hook
           (lambda ()
+            (git-gutter-mode t)
             (local-set-key (kbd "M-q") 'fill-paragraph)
             (local-set-key (kbd "C-,") 'my-switch-to-other-buffer)
             (local-set-key (kbd "C-o") 'open-line)
