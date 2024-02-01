23ff6cbc64a03e9fea75c7e0246dd2f891964dae
David Yeung <quxbaz@gmail.com>
2024-02-01 15:57:24 -0700

    modified:   my/bindings.el   Disable mouse functions.

--- my/bindings.el
+++ my/bindings.el
@@ -109,4 +109,9 @@
 
 
 ;; Misc
+(global-set-key (kbd "M-<down-mouse-1>") 'nil)
+(global-set-key (kbd "M-<drag-mouse-1>") 'nil)
+(global-set-key (kbd "<mouse-3>") 'nil)
+(global-set-key (kbd "M-<down-mouse-3>") 'nil)
+(global-set-key (kbd "M-<drag-mouse-3>") 'nil)
 (global-set-key [mouse-2] 'nil) ; Disable middle mouse button.
