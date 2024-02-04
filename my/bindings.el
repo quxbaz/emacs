015d96f376610a2bf928861d32f20f1a5e399a66
David Yeung <quxbaz@gmail.com>
2024-02-04 13:09:00 -0700

    modified:   my/bindings.el
    Rebind window keys.
    M-1     -> delete-other-windows
    C-[1-4] -> jump-to-register

--- my/bindings.el
+++ my/bindings.el
@@ -76,10 +76,11 @@
 
 ;; Windows
 (global-set-key (kbd "M-0") 'delete-window)
-(global-set-key (kbd "M-1") (lambda () (interactive) (jump-to-register (string-to-char "1"))))
-(global-set-key (kbd "M-2") (lambda () (interactive) (jump-to-register (string-to-char "2"))))
-(global-set-key (kbd "M-3") (lambda () (interactive) (jump-to-register (string-to-char "3"))))
-(global-set-key (kbd "M-4") (lambda () (interactive) (jump-to-register (string-to-char "4"))))
+(global-set-key (kbd "M-1") 'delete-other-windows)
+(global-set-key (kbd "C-1") (lambda () (interactive) (jump-to-register (string-to-char "1"))))
+(global-set-key (kbd "C-2") (lambda () (interactive) (jump-to-register (string-to-char "2"))))
+(global-set-key (kbd "C-3") (lambda () (interactive) (jump-to-register (string-to-char "3"))))
+(global-set-key (kbd "C-4") (lambda () (interactive) (jump-to-register (string-to-char "4"))))
 (global-set-key (kbd "s-x") 'window-swap-states)
 
 
