76a76305ec862773f2e0fae9dad7657ca6e41541
David Yeung <quxbaz@gmail.com>
2024-02-07 10:59:38 -0700

    modified:   my/theme.el   Switch theme -> doom-oceanic-next

--- my/theme.el
+++ my/theme.el
@@ -1,16 +1,46 @@
 ;; Theme, appearance, typography, colors
 
-;; Disables the current theme before loading the next one to avoid overlapping styles.
+
+;; Actual theme and font setting.
+(load-theme 'doom-oceanic-next t)
+(set-face-attribute 'default nil :font "Monaco-10")
+
+
+;; Disables the current theme before loading the next one to avoid overlapping
+;; styles. Don't use this to set the font. Use load-theme instead.
 (defun my/switch-theme (theme)
   (disable-theme (car custom-enabled-themes))
   (load-theme theme t))
 
-;; Theme
-(load-theme 'doom-dracula t)
-;; (load-theme 'doom-material t)
-;; (load-theme 'doom-monokai-pro t)
-;; (load-theme 'doom-oceanic-next t)
-;; (load-theme 'doom-tomorrow-day t)
+
+;; Keepers
+;; - Dark (dark background, high contrast)
+;; (my/switch-theme 'doom-challenger-deep)
+;; (my/switch-theme 'doom-dracula)
+;; (my/switch-theme 'doom-gruvbox)
+;; (my/switch-theme 'doom-henna)
+;; (my/switch-theme 'doom-monokai-classic)
+;; (my/switch-theme 'doom-monokai-pro)
+;; (my/switch-theme 'doom-moonlight)
+;; (my/switch-theme 'doom-oceanic-next)
+;; (my/switch-theme 'doom-one)
+;; (my/switch-theme 'doom-outrun-electric)
+;; (my/switch-theme 'doom-palenight)
+;; (my/switch-theme 'doom-snazzy)
+;; (my/switch-theme 'doom-vibrant)
+;; - Neutral (neutral background, medium contrast)
+;; (my/switch-theme 'doom-ephemeral)
+;; (my/switch-theme 'doom-material)
+;; (my/switch-theme 'doom-nord)
+;; (my/switch-theme 'doom-opera)
+;; (my/switch-theme 'doom-peacock)
+;; (my/switch-theme 'doom-wilmersdorf)
+;; (my/switch-theme 'doom-zenburn)
+;; - Light (light background, high contrast)
+;; (my/switch-theme 'doom-acario-light)
+;; (my/switch-theme 'doom-nord-light)
+;; (my/switch-theme 'doom-one-light)
+;; (my/switch-theme 'doom-tomorrow-day)
 
 
 ;; Fonts
@@ -23,7 +53,7 @@
 ;; (set-face-attribute 'default nil :font "Iosevka-10")           ;; Very skinny. Hard to read.
 ;; (set-face-attribute 'default nil :font "IosevkaComfyFixed-10") ;; A little nicer to look at.
 ;; (set-face-attribute 'default nil :font "JetBrainsMono-9")      ;; Kinda quirky.
-(set-face-attribute 'default nil :font "Monaco-10")            ;; Stylish.
+;; (set-face-attribute 'default nil :font "Monaco-10")            ;; Stylish.
 ;; (set-face-attribute 'default nil :font "Mononoki-10")          ;; Too condensed.
 ;; (set-face-attribute 'default nil :font "NotoSansMono-10")      ;; Good.
 ;; (set-face-attribute 'default nil :font "SFMono-10")            ;; Very Cool. Love it.
@@ -41,7 +71,7 @@
 
 
 ;; Colors
-(custom-set-faces
- `(default ((t (:background "#00242a"))))
- '(font-lock-comment-face ((t (:foreground "#999"))))
- '(magit-section-highlight ((t (:inherit hl-line :background "blue")))))
+;; (custom-set-faces
+;;  `(default ((t (:background "#00242a"))))
+;;  '(font-lock-comment-face ((t (:foreground "#999"))))
+;;  '(magit-section-highlight ((t (:inherit hl-line :background "blue")))))
