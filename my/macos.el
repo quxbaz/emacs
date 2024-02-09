e576ab95b46145deb1e8f6a7f2904977b3e0ad22
David Yeung <quxbaz@gmail.com>
2024-02-09 14:28:41 -0700

    modified:   my/macos.el

--- my/macos.el
+++ my/macos.el
@@ -1,6 +1,7 @@
 ;; Keybindings
 
-(setq mac-command-modifier 'meta)  ;; Map Command key as Meta.
-(setq mac-option-modifier 'super)  ;; Treat Option key as Super.
-(setq dired-use-ls-dired nil)      ;; Dired won't work otherwise.
-(setq mac-command-modifier 'meta)  ;; Bind Command key to Meta.
+(setq mac-command-modifier 'meta)    ;; Map Command key as Meta.
+(setq mac-option-modifier 'super)    ;; Treat Option key as Super.
+(setq dired-use-ls-dired nil)        ;; Dired won't work otherwise.
+(setq dired-listing-switches "-al")  ;; ^
+(setq mac-command-modifier 'meta)    ;; Bind Command key to Meta.
