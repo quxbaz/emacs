d7718cbfffff83ad67fb8e6d6bbf5b7bfcac93e5
David Yeung <quxbaz@gmail.com>
2024-02-11 10:56:55 -0700

    modified:   my/bindings.el   my/kmacros.el   Rename kmacro.

--- my/kmacros.el
+++ my/kmacros.el
@@ -1,6 +1,6 @@
 ;; Keyboard macros
 
 
-(defalias 'my/execute-prev-mx-command
+(defalias 'my/execute-last-mx-command
   (kmacro "M-x M-p <return>")
   "Executes the last M-x command. You should only invoke this with a binding.")
