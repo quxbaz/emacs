d2467d68b948826aa67c483afefb97eac80e06a3
David Yeung <quxbaz@gmail.com>
2024-02-11 10:32:22 -0700

    modified:   my/kmacros.el   Docstring.

--- my/kmacros.el
+++ my/kmacros.el
@@ -3,4 +3,4 @@
 
 (defalias 'my/execute-prev-mx-command
   (kmacro "M-x M-p M-p <return>")
-  "Executes the previous M-x command.")
+  "Executes the last M-x command. You should only invoke this with a binding.")
