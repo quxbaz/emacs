4cf54c71e4ba2294a2ba3ca45ed7344e836b1bc0
David Yeung <quxbaz@gmail.com>
2024-02-11 10:32:32 -0700

    modified:   my/kmacros.el   Fix kmacro.

--- my/kmacros.el
+++ my/kmacros.el
@@ -2,5 +2,5 @@
 
 
 (defalias 'my/execute-prev-mx-command
-  (kmacro "M-x M-p M-p <return>")
+  (kmacro "M-x M-p <return>")
   "Executes the last M-x command. You should only invoke this with a binding.")
