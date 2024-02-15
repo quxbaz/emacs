42a80ddb6d1754efa75a2d032ddac4f1e201a54f
David Yeung <quxbaz@gmail.com>
2024-02-15 14:33:46 -0700

    modified:   my/bindings.el   Comment.

--- my/bindings.el
+++ my/bindings.el
@@ -6,6 +6,9 @@
 ;; * Prefer `M` bindings for frequently-used actions.
 ;; * Prefer `C` bindings for destructive commands.
 ;; * Generally, Emacs convention suggests `M` for larger actions and `C` for smaller ones.
+;;
+;; Notes:
+;; - Available prefixes: C-q, C-z
 
 
 ;; Mouse config
