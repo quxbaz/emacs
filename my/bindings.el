73506a32f8ea68eaa4b88dc59efe7ec06f01936f
David Yeung <quxbaz@gmail.com>
2024-02-17 12:11:07 -0700

    modified:   my/bindings.el   Change comment heading format.

--- my/bindings.el
+++ my/bindings.el
@@ -11,7 +11,7 @@
 ;; - Available prefixes: C-q, C-z
 
 
-;; Disabled keys
+;; # Disabled keys
 (global-set-key (kbd "C-q") nil)
 (global-set-key (kbd "C-z") nil)
 ;; Disable these to force yourself to use your custom bindings.
@@ -20,7 +20,7 @@
 (global-unset-key (kbd "C-x e"))
 
 
-;; Mouse config
+;; # Mouse config
 (global-set-key (kbd "M-<down-mouse-1>") nil)
 (global-set-key (kbd "M-<drag-mouse-1>") nil)
 (global-set-key (kbd "<mouse-3>") nil)
@@ -31,59 +31,59 @@
 (global-set-key [mouse-9] 'switch-to-next-buffer)  ;; Bind to next button.
 
 
-;; Help, info
+;; # Help, info
 (global-set-key (kbd "C-h C-a") 'apropos)
 (global-set-key (kbd "C-h C-f") 'find-library)
 (global-set-key (kbd "C-h q") 'shortdoc-display-group)
 
 
-;; Mini-apps
+;; # Mini-apps
 (global-set-key (kbd "s-d") 'calendar)
 
 
-;; Appearance, themes
+;; # Appearance, themes
 ;;
 
 
-;; Text navigation, marking
+;; # Text navigation, marking
 (global-set-key (kbd "C-\\") 'my/match-paren)
 (global-set-key (kbd "M-.") 'my/mark-context)
 (global-set-key (kbd "M-h") 'my/mark-paragraph)
 
 
-;; Jumping
+;; # Jumping
 (global-set-key (kbd "M-'") 'xref-find-definitions)
 
 
-;; Editing
-;; Kill ring, clipboard, undo
+;; # Editing
+;; ## Kill ring, clipboard, undo
 (global-set-key [\S-insert] 'clipboard-yank)
 (global-set-key (kbd "C-M-/") 'undo-only)
-;; Creation, duplication, opening
+;; ## Creation, duplication, opening
 (global-set-key (kbd "C-o") 'my/open-line)
 (global-set-key (kbd "M-<return>") 'my/duplicate-line)
 (global-set-key (kbd "M-S-<return>") 'my/duplicate-block)
-;; Deletion, clearing
+;; ## Deletion, clearing
 (global-set-key (kbd "C-d") 'my/delete-char)
 (global-set-key (kbd "M-z") 'zap-up-to-char)
 (global-set-key (kbd "M-k") 'my/kill-block)
 (global-set-key (kbd "C-x C-k") 'kill-region)
 (global-set-key (kbd "C-M-k") 'my/clear-buffer)
-;; Whitespace
+;; ## Whitespace
 (global-set-key (kbd "M-\\") 'delete-horizontal-space)
 (global-set-key (kbd "C-x r \\") 'delete-whitespace-rectangle)
-;; Commenting
+;; ## Commenting
 (global-set-key (kbd "C-;") 'my/comment-line)
 (global-set-key (kbd "M-;") 'my/comment-block)
-;; Transposing
+;; ## Transposing
 (global-set-key (kbd "M-<up>") 'my/transpose-line)
 (global-set-key (kbd "M-<down>") (lambda () (interactive) (my/transpose-line t)))
 (global-set-key (kbd "M-T") 'transpose-regions)
-;; Sorting, alignment
+;; ## Sorting, alignment
 (global-set-key (kbd "C-c C-\\") 'align-regexp)
 
 
-;; Indentation
+;; # Indentation
 (global-set-key (kbd "M-q") 'my/indent-block)
 (global-set-key (kbd "M-I") 'indent-rigidly-left)
 (global-set-key (kbd "M-i") 'indent-rigidly-right)
@@ -91,45 +91,45 @@
 (global-set-key (kbd "C-j") 'newline-and-indent)
 
 
-;; Commands
+;; # Commands
 (global-set-key (kbd "C-.") 'repeat)
 
 
-;; Auto-complete
+;; # Auto-complete
 (global-set-key (kbd "M-/") 'hippie-expand)
 
 
-;; Search, replace, regexp, occur
-;; Search
+;; # Search, replace, regexp, occur
+;; ## Search
 (global-set-key (kbd "C-s") 'my/isearch-forward-dwim)
 (global-set-key (kbd "C-r") 'my/isearch-backward-dwim)
-;; Replace
+;; ## Replace
 (global-set-key (kbd "M-r") 'my/query-replace-dwim)
 (global-set-key (kbd "M-R") 'my/query-replace-buffer-dwim)
-;; Grep, find, occur
+;; ## Grep, find, occur
 (global-set-key (kbd "C-S-s") 'deadgrep)
 (global-set-key (kbd "C-c o") 'my/occur-dwim)
 
 
-;; Files
+;; # Files
 (global-set-key (kbd "M-F") 'find-file-other-window)
 (global-set-key (kbd "C-c p") 'find-file-at-point)
 
 
-;; Buffers, dired
-;; Buffers
+;; # Buffers, dired
+;; ## Buffers
 (global-set-key (kbd "M-s") 'save-buffer)
 (global-set-key (kbd "C-,") 'my/switch-to-other-buffer)
 (global-set-key (kbd "M-SPC") 'ido-switch-buffer)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-c C-v") 'my/revert-buffer)
-;; Dired
+;; ## Dired
 (global-set-key (kbd "C-c C-SPC") 'ido-dired)
 (global-set-key (kbd "M-`") 'dired-jump)
 (global-set-key (kbd "M-~") 'dired-jump-other-window)
 
 
-;; Registers
+;; # Registers
 (global-set-key (kbd "M-1") (lambda () (interactive) (jump-to-register ?1)))
 (global-set-key (kbd "M-2") (lambda () (interactive) (jump-to-register ?2)))
 (global-set-key (kbd "M-3") (lambda () (interactive) (jump-to-register ?3)))
@@ -137,12 +137,12 @@
 (global-set-key (kbd "C-M-m") 'my/move-line-to-register)
 
 
-;; Modes, viewing, narrowing, filtering
+;; # Modes, viewing, narrowing, filtering
 (global-set-key (kbd "C-<tab>") 'outline-toggle-children)
 (global-set-key (kbd "<C-iso-lefttab>") 'my/outline-toggle-all)
 
 
-;; Windows
+;; # Windows
 (global-set-key (kbd "M-o") 'other-window)
 (global-set-key (kbd "C-x C-o") 'other-window)
 (global-set-key (kbd "<escape> s") 'window-swap-states)
@@ -150,7 +150,7 @@
 (global-set-key (kbd "<escape> 1") 'delete-other-windows)
 
 
-;; Keyboard Macros
+;; # Keyboard Macros
 (global-set-key (kbd "<escape> [") 'kmacro-start-macro)
 (global-set-key (kbd "<escape> ]") 'kmacro-end-macro)
 (global-set-key (kbd "M-,") 'kmacro-end-and-call-macro)
@@ -163,7 +163,7 @@
 (global-set-key (kbd "C-c C-i") 'insert-kbd-macro)
 
 
-;; git, magit
+;; # git, magit
 (global-set-key (kbd "C-M-SPC") 'magit-status)
 (global-set-key (kbd "<f5>") 'magit-file-checkout)
 (global-set-key (kbd "C-c C-l") 'magit-file-dispatch)
