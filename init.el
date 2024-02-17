b02ec877a228bdec5de40da665639d148586a533
David Yeung <quxbaz@gmail.com>
2024-02-17 12:29:05 -0700

    modified:   init.el

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
 (load-file "~/.emacs.d/my/theme.el")
 (load-file "~/.emacs.d/my/macros.el")
 (load-file "~/.emacs.d/my/defuns.el")
+(load-file "~/.emacs.d/my/lisp-defuns.el")
 (load-file "~/.emacs.d/my/bindings.el")
 (load-file "~/.emacs.d/my/kmacros.el")
 (load-file "~/.emacs.d/my/hooks.el")
