36d41d0c59c96a986e3760f37408dff156af3c93
David Yeung <quxbaz@gmail.com>
2024-02-17 12:35:19 -0700

    modified:   my/lisp-defuns.el

--- my/lisp-defuns.el
+++ my/lisp-defuns.el
@@ -2,3 +2,68 @@
 ;;
 ;; defuns.el was starting to grow in size w/ a lot of that being lisp-related.
 ;; Hence, this file was created.
+
+
+(defun my/mark-list ()
+  (interactive)
+  (if (/= (char-after) ?\()
+      (backward-up-list 1 t t))
+  (mark-sexp))
+
+(defun my/eval-dwim ()
+  "Evals either the current region, block, or line - in that order of preference."
+  (interactive)
+  (if (use-region-p)
+      (eval-region (region-beginning) (region-end) t)
+    (save-excursion
+      (my/goto-root-list)
+      ;; If the point is at an opening parens, eval the list.
+      ;; Else-if point is to the right of a closing parens, eval the list.
+      ;; Else eval the line.
+      (cond ((looking-at "(")
+             (eval-region (point) (scan-lists (point) 1 0) t))
+            ((looking-back ")")
+             (eval-last-sexp nil))
+            (t
+             (eval-region (point-at-bol) (point-at-eol) t)))))
+  (my/flash-mode-line))
+
+;; BUG: This does not work inside strings.
+(defun my/eval-here ()
+  "Evaluates the most immediate list at point."
+  (interactive)
+  (let ((start (scan-lists (point) -1 1))
+        (end (scan-lists (point) 1 1)))
+    (eval-region start end t)))
+
+(defun my/lisp-kill-ring-save-dwim ()
+  (interactive)
+  (if (use-region-p)
+      (kill-ring-save nil nil t)
+    (save-excursion
+      (let ((n 0))
+        (while (and (< n 20)
+                    (not (eq (char-after) ?\()))
+          (condition-case nil
+              (backward-up-list 1 t)
+            (scan-error (if (looking-back ")")
+                            (backward-char))))
+          (setq n (+ n 1))))
+      (push-mark (point) t t)
+      (forward-list 1 t)
+      (kill-ring-save nil nil t)))
+  (message (car kill-ring)))
+
+(defun my/kill-list (arg)
+  (interactive "p")
+  ;; Kill the sexp, not the parent sexp when point is on a (.
+  (if (eq (char-after) ?\()
+      (forward-char))
+  (condition-case nil
+      (progn (backward-up-list) (kill-sexp))
+    (scan-error (my/kill-block arg))))
+
+(defun my/open-new-round ()
+  (interactive)
+  (paredit-close-round-and-newline)
+  (paredit-open-round))
