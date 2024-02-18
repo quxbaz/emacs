b50604847baffecaa4542a7d4f161400c6cc2219
David Yeung <quxbaz@gmail.com>
2024-02-18 14:45:20 -0700

    modified:   my/bindings.el   Rebind delete-other-windows -> [<escape> 9]

--- my/bindings.el
+++ my/bindings.el
@@ -147,7 +147,7 @@
 (global-set-key (kbd "C-x C-o") 'other-window)
 (global-set-key (kbd "<escape> s") 'window-swap-states)
 (global-set-key (kbd "<escape> 0") 'delete-window)
-(global-set-key (kbd "<escape> 1") 'delete-other-windows)
+(global-set-key (kbd "<escape> 9") 'delete-other-windows)
 
 
 ;; # Keyboard Macros
