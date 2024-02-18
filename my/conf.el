d3bf9e53ed0ab600b92884997eb08702f37c6e68
David Yeung <quxbaz@gmail.com>
2024-02-18 16:46:49 -0700

    modified:   my/conf.el   Add heading comment.

--- my/conf.el
+++ my/conf.el
@@ -1,33 +1,33 @@
 ;; Configuration, variables, global modes
 
 
-;; Startup
+;; # Startup
 (setq-default inhibit-startup-message t)
 (setq warning-minimum-level :emergency)  ;; Don't bombard me with plugin warnings and hijack focus.
 (add-to-list 'default-frame-alist '(fullscreen . maximized))  ;; Default to full-screen.
 (setq diary-file "~/personal/diary")
 
 
-;; Enable commands
+;; # Enable commands
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 (put 'set-goal-column 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
 
 
-;; Save screen space
+;; # Save screen space
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
 
-;; Screen, buffers, viewport
+;; # Screen, buffers, viewport
 (column-number-mode t)
 (global-hi-lock-mode t)
 (global-display-line-numbers-mode t)
 
 
-;; Indentation
+;; # Indentation
 (setq-default indent-tabs-mode nil)
 (setq indent-line-function 'insert-tab)  ;; Keep an eye on this, might cause problems.
 (setq-default tab-width 2)
@@ -35,29 +35,29 @@
 (setq-default sgml-basic-offset 2)
 
 
-;; Text, formatting, editing
+;; # Text, formatting, editing
 (setq kill-whole-line t)
 (setq-default truncate-lines t)
 (setq case-fold-search t)  ;; Search ignores case by default.
 (setq-default case-fold-search t)
 
 
-;; Parens
+;; # Parens
 (setq show-paren-delay 0)
 
 
-;; Visuals
+;; # Visuals
 (highlight-indent-guides-mode t)
 
 
-;; Search
+;; # Search
 (setq-default isearch-lazy-highlight-initial-delay 0)
 ;; Makes deadgrep start the search form the current directory instead of the project base.
 (defun my/get-current-dir () default-directory)
 (setq deadgrep-project-root-function #'my/get-current-dir)
 
 
-;; Safety, backups
+;; # Safety, backups
 (setq confirm-kill-emacs 'yes-or-no-p)
 (setq backup-directory-alist `((".*" . "~/.tmp")))
 (setq auto-save-file-name-transforms `((".*" "~/.tmp" t)))
@@ -67,21 +67,21 @@
 ;; (setq create-lockfiles nil)   ;; ^^
 
 
-;; Dired
+;; # Dired
 (setq wdired-allow-to-change-permissions t)  ;; Allow editing file modes.
 (setq dired-listing-switches "-laXGh --group-directories-first")  ;; Order directories first.
 
 
-;; Describe, help
+;; # Describe, help
 (setq help-window-select t)  ;; Focus describe buffers on load.
 
 
-;; System clipboard
+;; # System clipboard
 (setq x-select-enable-clipboard t)  ;; Allows you to copy into the system clipboard.
 (setq save-interprogram-paste-before-kill t)  ;; Save system clipboard to kill ring.
 
 
-;; Filename patterns
+;; # Filename patterns
 (add-to-list 'auto-mode-alist '("\\.js$" . javascript-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . javascript-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx?$" . javascript-mode))
@@ -90,7 +90,7 @@
 (add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
 
 
-;; Packages, modes
+;; # Packages, modes
 (setq uniquify-buffer-name-style 'forward)
 (ido-mode t)
 (show-paren-mode t)
@@ -101,26 +101,26 @@
 (yas-global-mode t)
 
 
-;; org-mode
+;; # org-mode
 (setq org-todo-keywords '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
 
 
-;; TRAMP
+;; # TRAMP
 ;; Not exactly sure what this is, but it enables me to actually use TRAMP.
 (setq tramp-terminal-type "tramp")
 ;; This line also needs to be in the REMOTE .zshrc file.
 ;; [[ $TERM == "tramp" ]] && unsetopt zle && PS1='$ ' && return
 
 
-;; Package / auto-complete
+;; # Package / auto-complete
 ;; (global-auto-complete-mode t)
 ;; (setq ac-auto-show-menu 0.5)
 
 
-;; Variables
+;; # Variables
 (custom-set-variables
  '(fill-column 80))
 
 
-;; Hardware
+;; # Hardware
 (setq printer-name "HLL2350DW")
