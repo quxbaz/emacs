8dafe1fe7d87e2c3195d2751b807883648c15f1b
David Yeung <quxbaz@gmail.com>
2024-02-18 14:45:55 -0700

    modified:   my/bindings.el   Reorder.

--- my/bindings.el
+++ my/bindings.el
@@ -146,8 +146,8 @@
 (global-set-key (kbd "M-o") 'other-window)
 (global-set-key (kbd "C-x C-o") 'other-window)
 (global-set-key (kbd "<escape> s") 'window-swap-states)
-(global-set-key (kbd "<escape> 0") 'delete-window)
 (global-set-key (kbd "<escape> 9") 'delete-other-windows)
+(global-set-key (kbd "<escape> 0") 'delete-window)
 
 
 ;; # Keyboard Macros
