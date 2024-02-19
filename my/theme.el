c8862be7929960e3758a330d6064dd2f142e844b
David Yeung <quxbaz@gmail.com>
2024-02-18 21:49:29 -0700

    modified:   my/theme.el

--- my/theme.el
+++ my/theme.el
@@ -3,7 +3,7 @@
 
 ;; Set theme and font.
 (load-theme 'modus-vivendi t)
-(set-face-attribute 'default nil :font "Monaco-10")
+;; (set-face-attribute 'default nil :font "Monaco-10")
 
 ;; Disables the current theme before loading the next one to avoid overlapping
 ;; styles. Don't use this to set the font. Use load-theme instead.
