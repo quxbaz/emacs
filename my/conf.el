0a6fcd4bbaadcf3a69053dfadc2f1a5aae7dba42
David Yeung <quxbaz@gmail.com>
2024-02-18 21:49:25 -0700

    modified:   my/conf.el

--- my/conf.el
+++ my/conf.el
@@ -16,9 +16,9 @@
 
 
 ;; # Save screen space
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+;; (menu-bar-mode -1)
+;; (tool-bar-mode -1)
+;; (scroll-bar-mode -1)
 
 
 ;; # Screen, buffers, viewport
