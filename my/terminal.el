aa25b1f0c78830cc96e8362fb0bef774f6a95a52
David Yeung <quxbaz@gmail.com>
2024-02-20 04:51:05 +0000

    modified:   my/terminal.el   Bind magit.

--- my/terminal.el
+++ my/terminal.el
@@ -4,3 +4,4 @@
 (global-set-key (kbd "<xterm-paste>") 'scroll-up-command)
 (global-set-key (kbd "M-,") 'my/switch-to-other-buffer)
 (global-set-key (kbd "M-RET") 'my/duplicate-line)
+(global-set-key (kbd "C-M-@") 'magit)
