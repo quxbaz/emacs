9af8096e74e35f328240259d62cba6bcd672d6b4
David Yeung <quxbaz@gmail.com>
2024-02-19 21:09:55 -0700

    modified:   my/theme.el

--- my/theme.el
+++ my/theme.el
@@ -5,14 +5,11 @@
 (load-theme 'modus-vivendi t)
 (set-face-attribute 'default nil :font "Monaco-10")
 
-;; # Colors
-(custom-set-faces
- ;; `(default ((t (:background "#00242a"))))
- ;; '(font-lock-comment-face ((t (:foreground "#999"))))
- ;; For certain themes which need a higher contrast line-nubmer.
- ;; '(line-number ((t (:inherit default :foreground "#999999" :strike-through nil :underline nil :slant normal :weight normal))))
- '(magit-section-highlight ((t (:inherit hl-line :background "blue")))))
 
+;; # Colors
+;; ## magit
+(custom-set-faces '(magit-section-highlight ((t (:inherit hl-line :background "blue")))))
+;; ## prism-mode
 (custom-set-variables
  '(prism-colors '("#1fceff" "#ff6bc4" "#ffc738" "#f95624"))
  '(prism-desaturations '(10 15 20))
@@ -20,9 +17,8 @@
  '(prism-comments nil)
  '(prism-strings nil)
  '(prism-level-1-strings ((t (:foreground "#999")))))
-
-(setq org-todo-keyword-faces '(("IN-PROGRESS" . "yellow")
-                               ("WAITING" . "orange")))
+;; ## org-mode
+(setq org-todo-keyword-faces '(("IN-PROGRESS" . "yellow") ("WAITING" . "orange")))
 
 
 ;; # Defuns
