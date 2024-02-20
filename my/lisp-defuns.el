1fdd434b7585a6c766d2cc09e33afff2ef7a76e3
David Yeung <quxbaz@gmail.com>
2024-02-19 21:59:06 -0700

    modified:   my/lisp-defuns.el   Comment.

--- my/lisp-defuns.el
+++ my/lisp-defuns.el
@@ -1,7 +1,4 @@
-;; Lisp-related function definitions
-;;
-;; defuns.el was starting to grow in size w/ a lot of that being lisp-related.
-;; Hence, this file was created.
+;; Like defuns.el, but for lisp-related stuff.
 
 
 (defun my/mark-list ()
