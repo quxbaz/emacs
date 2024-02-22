ae8f06d6a13ea4d8c271af5529e554027d7cc021
David Yeung <quxbaz@gmail.com>
2024-02-22 09:29:28 -0700

    modified:   my/terminal.el   Bind my/revert-buffer.

--- my/terminal.el
+++ my/terminal.el
@@ -4,4 +4,5 @@
 (global-set-key (kbd "<xterm-paste>") 'scroll-up-command)
 (global-set-key (kbd "M-,") 'my/switch-to-other-buffer)
 (global-set-key (kbd "M-RET") 'my/duplicate-line)
+(global-set-key (kbd "C-c <xterm-paste>") 'my/revert-buffer)
 (global-set-key (kbd "C-M-@") 'magit)
