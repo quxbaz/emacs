a3e5bf03a59199389c63926d2c9225592a84d26a
David Yeung <quxbaz@gmail.com>
2024-02-22 15:44:29 -0700

    modified:   scratch.el

--- scratch.el
+++ scratch.el
@@ -2,5 +2,5 @@
 (define-minor-mode my/scratch-mode "my/scratch-mode" nil nil (make-sparse-keymap))
 (define-key my/scratch-mode-map (kbd "C-c C-c") 'eros-eval-defun)
 (define-key my/scratch-mode-map (kbd "C-c .") 'eros-eval-last-sexp)
-(my/scratch-mode )
+(my/scratch-mode t)
 ;;
