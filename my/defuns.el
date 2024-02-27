cc1c96dc2b65a15a35e0930b7b499b35ac049497
David Yeung <quxbaz@gmail.com>
2024-02-26 22:06:34 -0700

    modified:   my/defuns.el   Simplify my/occur-dwim.

--- my/defuns.el
+++ my/defuns.el
@@ -204,15 +204,11 @@ region as the search string."
 
 (defun my/occur-dwim ()
   (interactive)
-  (let ((short-word (current-word nil t))
-        (long-word  (current-word nil nil)))
-    (if (and (use-region-p)
-             (or (string= (my/region-text) short-word)
-                 (string= (my/region-text) long-word)))
-        (let ((text (my/region-text)))
-          (deactivate-mark)
-          (occur text))
-      (call-interactively 'occur))))
+  (if (use-region-p)
+      (progn
+        (deactivate-mark)
+        (occur (my/region-text)))
+    (call-interactively 'occur)))
 
 
 ;; # dwim region commands
