3de50ca5b6aaa9010d6a51ebc19ea66b5995d937
David Yeung <quxbaz@gmail.com>
2024-06-14 17:25:31 -0600

    modified:   scratch.el

--- scratch.el
+++ scratch.el
@@ -7,3 +7,11 @@
 ;; my/eval-dwim and my/eval-here don't work.
 (setq-local foo '(1 2 42 42 3 4))
 foo
+;;
+
+
+;; Fix this test case. When point is at:
+;;     '(1 2 |42 42 3 4)
+;; my/wrap-sexp wraps the sexp prior to the current one.
+(setq-local foo '(1 2 42 42 3 4))
+;;
