dd37d4dbee2bd04915f18f058479b1cf1284404b
David Yeung <quxbaz@gmail.com>
2024-06-14 17:21:37 -0600

    modified:   scratch.el

--- scratch.el
+++ scratch.el
@@ -1,2 +1,9 @@
-;; Scratch buffer for testing out code.
+;; Scratch buffer for testing out code
 ;;
+                                        ;
+
+;; Fix this test case. When point is at:
+;;     fo|o
+;; my/eval-dwim and my/eval-here don't work.
+(setq-local foo '(1 2 42 42 3 4))
+foo
