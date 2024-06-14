e11e89ac5f16cef68c19b8bc7aeeb678489fdedd
David Yeung <quxbaz@gmail.com>
2024-06-14 17:18:43 -0600

    modified:   scratch.el

--- scratch.el
+++ scratch.el
@@ -1,10 +1,2 @@
-;; Keep. Eval.
-(define-minor-mode my/scratch-mode "my/scratch-mode" nil nil (make-sparse-keymap))
-(define-key my/scratch-mode-map (kbd "C-c C-c") 'eros-eval-defun)
-(define-key my/scratch-mode-map (kbd "C-c .") 'eros-eval-last-sexp)
-(my/scratch-mode t)
+;; Scratch buffer for testing out code.
 ;;
-(define-key my/scratch-mode-map (kbd "C-c C-c") 'my/eval-dwim)
-(define-key my/scratch-mode-map (kbd "C-c .") 'my/eval-here)
-;;
-
