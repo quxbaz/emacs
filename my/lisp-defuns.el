19011cc63027f1669ca7b7881bcae2dc8e21450c
David Yeung <quxbaz@gmail.com>
2024-06-14 17:03:52 -0600

    modified:   my/lisp-defuns.el

--- my/lisp-defuns.el
+++ my/lisp-defuns.el
@@ -139,6 +139,6 @@ Also works from inside strings."
 (defun my/transpose-chars ()
   "Like transpose-chars, but calls transpose-sexps if point is on an opening delimiter."
   (interactive)
-  (if (looking-at "\\s\(")
+  (if (my/is-at-opening-parens)
       (call-interactively 'transpose-sexps)
     (call-interactively 'transpose-chars)))
