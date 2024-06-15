ca1e0ba18478cc90ecd2d6c2919653985b1210fb
David Yeung <quxbaz@gmail.com>
2024-06-15 17:43:19 -0600

    new file:   bugs.el

---
+++ bugs.el
@@ -0,0 +1,26 @@
+;; A record of bugs.
+;;
+;;
+
+
+;; When point is at:
+;;     (setq-local foo '(1 2|3))
+;; my/eval-dwim doesn't work.
+(setq-local foo '(1 2 3))
+foo
+;;
+
+
+;; When point is at:
+;;     fo|o
+;; my/eval-here doesn't work properly. It should eval the symbol foo.
+(setq-local foo '(1 2 3))
+foo
+;;
+
+
+;; Fix this test case. When point is at:
+;;     '(1 2 |42 42 3 4)
+;; my/wrap-sexp wraps the sexp prior to the current one.
+(setq-local foo '(1 2 42 42 3 4))
+;;
