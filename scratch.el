f7004c3227931812e036ebb77e59d2fbcd0355bf
David Yeung <quxbaz@gmail.com>
2024-06-15 17:43:16 -0600

    modified:   scratch.el

--- scratch.el
+++ scratch.el
@@ -1,17 +1,3 @@
 ;; Scratch buffer for testing out code
 ;;
-                                        ;
-
-;; Fix this test case. When point is at:
-;;     fo|o
-;; my/eval-dwim and my/eval-here don't work.
-(setq-local foo '(1 2 42 42 3 4))
-foo
-;;
-
-
-;; Fix this test case. When point is at:
-;;     '(1 2 |42 42 3 4)
-;; my/wrap-sexp wraps the sexp prior to the current one.
-(setq-local foo '(1 2 42 42 3 4))
 ;;
