733c987710366aea8dc07169e9b1321fec5e056e
David Yeung <quxbaz@gmail.com>
2024-06-15 18:15:34 -0600

    modified:   bugs.el

--- bugs.el
+++ bugs.el
@@ -4,15 +4,29 @@
 
 
 ;; When point is at:
-;;     fo|o
+;;   fo|o
 ;; my/eval-here doesn't work properly. It should eval the symbol foo.
 (setq-local foo '(1 2 3))
 foo
 ;;
 
 
-;; Fix this test case. When point is at:
-;;     '(1 2 |42 42 3 4)
+;; When point is at:
+;;   '(1 2 |42 42 3 4)
 ;; my/wrap-sexp wraps the sexp prior to the current one.
 (setq-local foo '(1 2 42 42 3 4))
+;;
+
+
+;; When point is at:
+;;   (|setq-local foo '(1 2 42 42 3 4))
+;; my/wrap-sexp should wrap setq-local, not the entire list.
+(setq-local foo '(1 2 42 42 3 4))
+;;
+
+
+;; When point is at
+;;   (looking-back "[[:blan|k:]]")
+;; my/open-new-round doesn't work.
+(looking-back "[[:blank:]]")
 ;;
