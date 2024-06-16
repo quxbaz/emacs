c09eda57dd526c5d136cdc026113cc22b2900e44
David Yeung <quxbaz@gmail.com>
2024-06-15 18:27:48 -0600

    modified:   bugs.el

--- bugs.el
+++ bugs.el
@@ -18,13 +18,6 @@ foo
 ;;
 
 
-;; When point is at:
-;;   (|setq-local foo '(1 2 42 42 3 4))
-;; my/wrap-sexp should wrap setq-local, not the entire list.
-(setq-local foo '(1 2 42 42 3 4))
-;;
-
-
 ;; When point is at
 ;;   (looking-back "[[:blan|k:]]")
 ;; my/open-new-round doesn't work.
