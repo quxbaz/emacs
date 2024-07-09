e8590d6e563e0c3f11f474e1b85a8ed5ab8536e5
David Yeung <quxbaz@gmail.com>
2024-07-09 11:14:50 -0600

    modified:   my/terminal.el

--- my/terminal.el
+++ my/terminal.el
@@ -3,6 +3,6 @@
 
 (global-set-key (kbd "<xterm-paste>") 'scroll-up-command)
 (global-set-key (kbd "M-,") 'my/switch-to-other-buffer)
-(global-set-key (kbd "M-RET") 'my/duplicate-line)
+(global-set-key (kbd "M-RET") 'my/duplicate-list)
 (global-set-key (kbd "C-c <xterm-paste>") 'my/revert-buffer)
 (global-set-key (kbd "C-M-@") 'magit)
