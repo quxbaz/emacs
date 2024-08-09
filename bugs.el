60b3aca2b36b05ac324e65a7cc714ab1acf1494f
David Yeung <quxbaz@gmail.com>
2024-08-09 09:46:02 -0600

    modified:   bugs.el

--- bugs.el
+++ bugs.el
@@ -8,6 +8,7 @@
 ;; eval-dwim should eval the entire list and not the line.
 ;; Don't eval lines, period. No point. Use syntax-ppss to find the opening parens and eval from there.
 ;; Be careful of point position. Always eval from where point began before invocation.
+;; Make sure to check if you're in a list already before going down this path.
 (propertize "hello world"
             'face '(:foreground "red" :weight "bold")
             'mouse-face 'highlight)
