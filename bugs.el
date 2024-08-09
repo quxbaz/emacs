0284ff8b276013a352c99deb382d7fee2cc222c6
David Yeung <quxbaz@gmail.com>
2024-08-09 09:45:15 -0600

    modified:   bugs.el

--- bugs.el
+++ bugs.el
@@ -1,3 +1,13 @@
 ;; A record of bugs.
 ;;
 ;;
+
+;; Bug: eval-dwim
+;; When point is at:
+;;   'mouse-face 'highlight)|
+;; eval-dwim should eval the entire list and not the line.
+;; Don't eval lines, period. No point. Use syntax-ppss to find the opening parens and eval from there.
+;; Be careful of point position. Always eval from where point began before invocation.
+(propertize "hello world"
+            'face '(:foreground "red" :weight "bold")
+            'mouse-face 'highlight)
