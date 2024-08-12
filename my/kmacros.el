c4dafec5ed0fb8e8947196354f2776a8ca86a30e
David Yeung <quxbaz@gmail.com>
2024-08-12 14:57:54 -0600

    modified:   my/kmacros.el

--- my/kmacros.el
+++ my/kmacros.el
@@ -4,8 +4,9 @@
 
 
 (defalias 'eval-last-expr
-  (kmacro "M-: M-p" 4 "%d"))
+  (kmacro "M-: M-p" 4 "%d")
+  "Executes the most recent eval-expression string.")
 
 (defalias 'my/execute-last-mx-command
   (kmacro "M-x M-p <return>")
-  "Executes the last M-x command. You should only invoke this with a binding.")
+  "Executes the most recent M-x command. You should only invoke this with a binding.")
