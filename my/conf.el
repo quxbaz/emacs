cfd935b0424f578c530b80b149b501c09bba5ba6
David Yeung <quxbaz@gmail.com>
2024-08-26 23:48:03 -0600

    modified:   my/conf.el

--- my/conf.el
+++ my/conf.el
@@ -85,6 +85,7 @@
 
 
 ;; # Filename patterns
+(add-to-list 'auto-mode-alist '("\\.cljs$" . emacs-lisp-mode))
 (add-to-list 'auto-mode-alist '("\\.js$" . javascript-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . javascript-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx?$" . javascript-mode))
