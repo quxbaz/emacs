65d2cc459cc7a42237af56b7e8ae3a3e46bb4e2b
David Yeung <quxbaz@gmail.com>
2024-09-05 10:25:33 -0600

    modified:   my/lisp-conf.el

--- my/lisp-conf.el
+++ my/lisp-conf.el
@@ -54,7 +54,7 @@
 ;;
 ;; # Common Lisp config
 (setq inferior-lisp-program (executable-find "sbcl"))
-(keymap-set lisp-mode-map "<escape> RET" 'my/visit-slime-repl)
+(keymap-set lisp-mode-map "<escape> ;" 'my/visit-slimej-repl)
 
 
 ;;
