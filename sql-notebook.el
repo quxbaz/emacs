41f65c1f6ff192e4dfe0a9a79b8c8f7c28d47479
David Yeung <quxbaz@gmail.com>
2024-09-18 16:32:00 -0600

    new file:   sql-notebook.el

---
+++ sql-notebook.el
@@ -0,0 +1,22 @@
+(sql-connect "wnmu-edu-db")
+
+(my/sql-command 'help)
+(my/sql-command 'status)
+(my/sql-show-databases)
+(my/sql-show-tables)
+
+;; TODO: Create a snippet for this.
+(my/sql-select '((columns . *)
+                 (table . wp_10_postmeta)
+                 (where . nil)
+                 (order-by . meta_key)
+                 (limit . 10)))
+
+;; TODO: Support list in 'columns prop.
+(my/sql-select '((columns . (meta_id meta_key))
+                 (table . wp_10_postmeta)
+                 (limit . 10)))
+
+(prin1-to-string '(meta_id meta_key))
+
+(sql-send-string "SELECT * FROM wp_10_postmeta LIMIT 10")
