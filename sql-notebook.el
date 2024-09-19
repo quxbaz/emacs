717f2bb2a28ebb822ed31a052aca60a9c32d2354
David Yeung <quxbaz@gmail.com>
2024-09-19 17:14:48 -0600

    modified:   sql-notebook.el

--- sql-notebook.el
+++ sql-notebook.el
@@ -3,18 +3,17 @@
 ;; End:
 
 (sql-connect "wnmu-edu-db")
-
-(my/sql-command 'help)
-(my/sql-command 'status)
+(my/sql-physical-eval "help")
+(my/sql-physical-eval "status")
 (my/sql-show-databases)
 (my/sql-show-tables)
 
 ;; TODO: Create a snippet for this.
-(my/sql-query '((columns . *)
-                (table . wp_10_postmeta)
-                (where . nil)
-                (order-by . meta_key)
-                (limit . 10)))
+(my/sql-select '((columns . *)
+                 (table . wp_10_postmeta)
+                 (where . nil)
+                 (order-by . meta_key)
+                 (limit . 10)))
 
 ;; TODO: Support list in 'columns prop.
 (my/sql-select '((columns . (meta_id meta_key))
