dedab951af8867e93c21fde42aa23af230affc76
David Yeung <quxbaz@gmail.com>
2024-10-03 11:32:58 -0600

    modified:   my/lisp-conf.el   Unbind M-(

--- my/lisp-conf.el
+++ my/lisp-conf.el
@@ -46,6 +46,7 @@
 
 ;; Override default paredit keymap.
 (eval-after-load 'paredit '(progn
+                             (keymap-set paredit-mode-map "M-(" (my/cmd nil))
                              (keymap-set paredit-mode-map "C-j" 'newline-and-indent)
                              (keymap-set paredit-mode-map "M-s" 'save-buffer)
                              (keymap-set paredit-mode-map "M-r" 'my/query-replace-dwim)
