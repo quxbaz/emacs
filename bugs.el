82eb4e66a0cd27776e7b5025b120b20a709b6f4b
David Yeung <quxbaz@gmail.com>
2024-10-15 14:50:44 -0600

    modified:   bugs.el

--- bugs.el
+++ bugs.el
@@ -1,3 +1,10 @@
 ;; A record of bugs.
 ;;
 ;;
+
+;; `sl` snippet expand doesn't work.
+;; Try sl <expand> at first and last line.
+;; It should expand to:
+;;   (setq-local |)
+;; But instead expands to
+;;   setq-local |
