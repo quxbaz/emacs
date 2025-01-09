54b8f98819f6b4652560f7a980635425c74aae8e
David Yeung <quxbaz@gmail.com>
2025-01-09 12:59:37 -0700

    modified:   my/conf.el

--- my/conf.el
+++ my/conf.el
@@ -8,6 +8,46 @@
 (setq diary-file "~/personal/diary")
 
 
+;; Data
+(setq my/init-files (list user-init-file
+                          (concat user-emacs-directory "bugs.el")
+                          (concat user-emacs-directory "scratch.el")
+                          (concat user-emacs-directory "sql-notebook.el")
+                          "~/personal/automotive.org"
+                          "~/personal/bookmarks.org"
+                          "~/personal/cad.org"
+                          "~/personal/diary"
+                          "~/personal/dogs.org"
+                          "~/personal/food.org"
+                          "~/personal/main.org"
+                          "~/personal/media-queue.org"
+                          "~/personal/programming.org"
+                          "~/personal/promise.org"
+                          "~/personal/rem.org"
+                          "~/personal/rem-structured.org"
+                          "~/personal/archive/buy.org"
+                          "~/personal/archive/people.org"
+                          "~/personal/archive/projects.org"
+                          "~/personal/automotive/subaru-outback-2008/Notes.org"
+                          "~/personal/automotive/subaru-outback-2008/log.org"
+                          "~/personal/exercise/exercises.org"
+                          "~/personal/exercise/misc.org"
+                          "~/personal/exercise/movement.org"
+                          "~/personal/exercise/records.org"
+                          "~/personal/exercise/routines.org"
+                          "~/personal/linux/emacs.org"
+                          "~/personal/linux/linux.org"
+                          "~/conf/sync.org"
+                          "~/conf/i3/config"
+                          "~/conf/zsh/.zshrc"
+                          "~/work/wnmu/Todo.org"
+                          "~/work/wnmu/huddle-notes.org"
+                          "~/work/wnmu/projects.org"
+                          "~/work/wnmu/records/creds.org"
+                          "~/work/wnmu/records/qr-code.org"
+                          "~/work/wnmu/refactor.org"))
+
+
 ;; Logging
 (setq message-log-max 10000)
 
