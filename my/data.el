0fc0baec90ccbc5db614c164a5850fad498a4e17
David Yeung <quxbaz@gmail.com>
2025-01-14 10:38:46 -0700

    new file:   my/data.el

---
+++ my/data.el
@@ -0,0 +1,40 @@
+;; Data
+
+
+(setq my/init-files (list user-init-file
+                          (concat user-emacs-directory "bugs.el")
+                          (concat user-emacs-directory "scratch.el")
+                          (concat user-emacs-directory "sql-notebook.el")
+                          "~/personal/automotive.org"
+                          "~/personal/bookmarks.org"
+                          "~/personal/cad.org"
+                          "~/personal/diary"
+                          "~/personal/dogs.org"
+                          "~/personal/food.org"
+                          "~/personal/main.org"
+                          "~/personal/media-queue.org"
+                          "~/personal/programming.org"
+                          "~/personal/promise.org"
+                          "~/personal/rem.org"
+                          "~/personal/rem-structured.org"
+                          "~/personal/archive/buy.org"
+                          "~/personal/archive/people.org"
+                          "~/personal/archive/projects.org"
+                          "~/personal/automotive/subaru-outback-2008/Notes.org"
+                          "~/personal/automotive/subaru-outback-2008/log.org"
+                          "~/personal/exercise/exercises.org"
+                          "~/personal/exercise/misc.org"
+                          "~/personal/exercise/movement.org"
+                          "~/personal/exercise/records.org"
+                          "~/personal/exercise/routines.org"
+                          "~/personal/linux/emacs.org"
+                          "~/personal/linux/linux.org"
+                          "~/conf/sync.org"
+                          "~/conf/i3/config"
+                          "~/conf/zsh/.zshrc"
+                          "~/work/wnmu/Todo.org"
+                          "~/work/wnmu/huddle-notes.org"
+                          "~/work/wnmu/projects.org"
+                          "~/work/wnmu/records/creds.org"
+                          "~/work/wnmu/records/qr-code.org"
+                          "~/work/wnmu/refactor.org"))
