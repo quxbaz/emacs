452b964afb3dca0f0487dd1fe4244dc01a240916
David Yeung <quxbaz@gmail.com>
2025-08-04 18:34:49 -0600

    modified:   my/conf.el

--- my/conf.el
+++ my/conf.el
@@ -101,7 +101,19 @@
 (add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
 
 
-;; # Packages, modes
+;; # org-mode
+(setq org-adapt-indentation t)  ;; Indent after headings.
+;; Add org-agenda files.
+(custom-set-variables
+ '(org-agenda-files '("~/work/wnmu/Todo.org")))
+(custom-set-faces
+ '(org-ellipsis ((t (:foreground "gray50")))))
+(setq org-ellipsis " [...]")
+(setq org-todo-keywords '((sequence "TODO"              "NEXT"                 "IN-PROGRESS"             "WAITING"              "FAILED"                "QUESTION"              "REVIEW"              "LIMBO"              "BACKLOG"      "|"     "NOTE"              "DONE-INT"                  "DONE")))
+(setq org-todo-keyword-faces     '(("TODO" . "yellow") ("NEXT" . "OrangeRed") ("IN-PROGRESS" . "cyan1") ("WAITING" . "orange") ("FAILED" . "DeepPink") ("QUESTION" . "grey50") ("REVIEW" . "orchid") ("LIMBO" . "grey50") ("BACKLOG" . "grey50") ("NOTE" . "grey50") ("DONE-INT" . "PaleGreen2") ("DONE" . "green")))
+
+
+;; # Packages, modes, misc
 (setq uniquify-buffer-name-style 'forward)
 (setq ivy-do-completion-in-region nil)
 (ivy-mode t)
@@ -113,18 +125,6 @@
 (global-corfu-mode t)
 (setq blink-matching-paren nil) ;; Disable paredit delay when closing round.
 (setq calc-algebraic-mode t)
-
-
-;; # org-mode
-(setq org-adapt-indentation t)  ;; Indent after headings.
-;; Add org-agenda files.
-(custom-set-variables
- '(org-agenda-files '("~/work/wnmu/Todo.org")))
-(custom-set-faces
- '(org-ellipsis ((t (:foreground "gray50")))))
-(setq org-ellipsis " [...]")
-(setq org-todo-keywords '((sequence "TODO"              "NEXT"                 "IN-PROGRESS"             "WAITING"              "FAILED"                "QUESTION"              "REVIEW"              "LIMBO"              "BACKLOG"      "|"     "NOTE"              "DONE-INT"                  "DONE")))
-(setq org-todo-keyword-faces     '(("TODO" . "yellow") ("NEXT" . "OrangeRed") ("IN-PROGRESS" . "cyan1") ("WAITING" . "orange") ("FAILED" . "DeepPink") ("QUESTION" . "grey50") ("REVIEW" . "orchid") ("LIMBO" . "grey50") ("BACKLOG" . "grey50") ("NOTE" . "grey50") ("DONE-INT" . "PaleGreen2") ("DONE" . "green")))
 
 
 ;; # TRAMP
