c977321f4e160e56a692f22edaab34dfa04baa13
David Yeung <quxbaz@gmail.com>
2025-08-08 14:03:49 -0600

    Revert "modified:   init.el   Save filter groups."
    This reverts commit ccf293624a5caf7a12441ffc6f8224010bd8cfd0.

--- init.el
+++ init.el
@@ -35,20 +35,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(fill-column 80)
- '(ibuffer-saved-filter-groups
-   '(("filters"
-      ("Todos" (filename . "todo.org"))
-      ("resist" (not starred-name) (not name . "magit") (not used-mode . dired-mode) (directory . "/resist/")))))
- '(ibuffer-saved-filters
-   '(("programming" (or (derived-mode . prog-mode) (mode . ess-mode) (mode . compilation-mode)))
-     ("text document" (and (derived-mode . text-mode) (not (starred-name))))
-     ("TeX" (or (derived-mode . tex-mode) (mode . latex-mode) (mode . context-mode)
-                (mode . ams-tex-mode) (mode . bibtex-mode)))
-     ("web" (or (derived-mode . sgml-mode) (derived-mode . css-base-mode)
-                (derived-mode . js-base-mode) (derived-mode . typescript-ts-base-mode)
-                (mode . js2-mode) (derived-mode . haml-mode) (mode . sass-mode)))
-     ("gnus" (or (mode . message-mode) (mode . mail-mode) (mode . gnus-group-mode)
-                 (mode . gnus-summary-mode) (mode . gnus-article-mode)))))
  '(package-selected-packages
    '(
      aggressive-indent
