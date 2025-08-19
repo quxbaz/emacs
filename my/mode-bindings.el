8c80984f76db7dcb2c020969f3b4b859c3b63181
David Yeung <quxbaz@gmail.com>
2025-08-19 15:07:37 -0600

    modified:   my/mode-bindings.el

--- my/mode-bindings.el
+++ my/mode-bindings.el
@@ -10,8 +10,7 @@
 
 (eval-after-load 'calc
   '(progn
-     (keymap-set calc-mode-map "SPC" 'calc-algebraic-entry)
-     (keymap-set calc-mode-map "e" 'calc-power)))
+     (keymap-set calc-mode-map "SPC" 'calc-algebraic-entry)))
 
 (eval-after-load 'calendar
   '(progn
