102767626eac7f90bddbd5b872aa312fcd22eabb
David Yeung <quxbaz@gmail.com>
2025-08-27 21:51:05 -0600

    new file:   calc.el

---
+++ calc.el
@@ -0,0 +1,6 @@
+
+;;; Mode settings stored by Calc on Wed Aug 27 21:50:28 2025
+(setq calc-display-trail nil)
+;; (setq calc-algebraic-mode t)
+;; (setq calc-simplify-mode 'none)  ;; Disable automatic simplification.
+;;; End of mode settings
