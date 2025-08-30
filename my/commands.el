4ab1266acd8cb867ebdccb45e75a31525479c052
David Yeung <quxbaz@gmail.com>
2025-08-30 16:56:30 -0600

    modified:   my/commands.el

--- my/commands.el
+++ my/commands.el
@@ -662,7 +662,15 @@ DOWN? [bool] [default = t]    If true, transposes the line downwards."
 
 ;; calc-mode
 
-(defun my/calc-history-prev ()
+(defun my/calc-edit-spc ()
+  "Like SPC, but exits the buffer if content is empty. This is useful for
+quickly exiting the edit buffer if you accidentally brought it up."
+  (interactive)
+  (if (= (length (string-trim (buffer-substring-no-properties (point-min) (point-max)))) 68)
+      (kill-current-buffer)
+    (call-interactively 'self-insert-command)))
+
+(defun my/calc-edit-history-prev ()
   "Recall previous calc history entry."
   (interactive)
   (when calc-alg-entry-history
@@ -671,7 +679,7 @@ DOWN? [bool] [default = t]    If true, transposes the line downwards."
       (goto-char (point-min)) (forward-line 2) (delete-region (point) (point-max))
       (insert (nth my/calc-history-index calc-alg-entry-history)))))
 
-(defun my/calc-history-next ()
+(defun my/calc-edit-history-next ()
   "Recall next calc history entry."
   (interactive)
   (when calc-alg-entry-history
