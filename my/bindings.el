a2808c9baa2789944e34eb50e0b4497d92822559
David Yeung <quxbaz@gmail.com>
2025-09-05 13:21:26 -0600

    modified:   my/bindings.el

--- my/bindings.el
+++ my/bindings.el
@@ -182,8 +182,9 @@
 (global-set-key (kbd "C-x C-o") 'my/other-window)
 (global-set-key (kbd "C-u C-s") (my/cmd (split-window-right) (windmove-right)))
 (global-set-key (kbd "<escape> -") 'window-swap-states)
-(global-set-key (kbd "<escape> .") 'delete-other-windows)
-(global-set-key (kbd "<escape> \\") 'delete-window)
+(global-set-key (kbd "<escape> .") 'delete-window)
+;; (global-set-key (kbd "<escape> \\") 'delete-window)
+;; (global-set-key (kbd "<escape> .") 'delete-other-windows)
 (global-set-key (kbd "<escape> = ") 'balance-windows)
 ;; ## Dired
 (global-set-key (kbd "M-`") 'my/dired-jump)
