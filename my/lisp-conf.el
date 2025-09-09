bf5537dd8376d5d25cce9894a8f1576a1855dd99
David Yeung <quxbaz@gmail.com>
2025-09-09 16:19:35 -0600

    modified:   my/lisp-conf.el

--- my/lisp-conf.el
+++ my/lisp-conf.el
@@ -49,7 +49,7 @@
 ;; the future (it will see all of the libraries you install, completion will
 ;; work etc).
 ;; (load (expand-file-name "~/quicklisp/slime-helper.el"))
-(keymap-set lisp-mode-map "<escape> ;" 'slime-switch-to-output-buffer)
+(keymap-set lisp-mode-map "M-S-SPC" 'slime-switch-to-output-buffer)
 
 ;;
 ;; # Emacs Lisp (Elisp) config
