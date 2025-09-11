894c22780250e56dc1eb65f330b026a47dc8b125
David Yeung <quxbaz@gmail.com>
2025-09-10 22:29:49 -0600

    new file:   my/packages.el

---
+++ my/packages.el
@@ -0,0 +1,9 @@
+;; Packages
+
+
+(use-package claude-code-ide
+  :vc (:url "https://github.com/manzaltu/claude-code-ide.el" :rev :newest)
+  :bind ("C-c C-y" . claude-code-ide-menu) ; Set your favorite keybinding
+  :config
+  ;; (claude-code-ide-emacs-tools-setup)
+  ) ; Optionally enable Emacs MCP tools
