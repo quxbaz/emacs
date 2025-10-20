cfe74d566b282f5a4705e52a5983afb4294c3377
David Yeung <quxbaz@gmail.com>
2025-10-19 22:03:30 -0600

    modified:   my/mode-bindings.el

--- my/mode-bindings.el
+++ my/mode-bindings.el
@@ -34,7 +34,7 @@
          (:map calc-mode-map ("j e" . my/calc-sel-jump-equals))
          (:map calc-mode-map ("j l" . calc-commute-left))
          (:map calc-mode-map ("j r" . calc-commute-right))
-         (:map calc-mode-map ("j k" . calc-sel-commute))
+         (:map calc-mode-map ("X" . calc-sel-commute))
          (:map calc-mode-map ("j n" . calc-sel-negate))))
 
 (add-hook 'calc-edit-mode-hook
