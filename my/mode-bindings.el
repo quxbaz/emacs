2749e116d8e6eee1148e43e5847131c37b43d635
David Yeung <quxbaz@gmail.com>
2025-11-12 11:57:23 -0700

    modified:   my/mode-bindings.el

--- my/mode-bindings.el
+++ my/mode-bindings.el
@@ -49,6 +49,7 @@
          (:map calc-mode-map ("N" . calc-sel-negate))
          (:map calc-mode-map ("C-d" . calc-del-selection))
          ;; Statistics and vectors
+         (:map calc-mode-map ("," . my/calc-key-comma))
          (:map calc-mode-map ("F" . calc-reduce))
          (:map calc-mode-map ("k t" . calc-perm))
          (:map calc-mode-map ("M" . calc-map))
