e59b89160ddc466b75b761d976695b7d80c18976
David Yeung <quxbaz@gmail.com>
2025-11-19 13:08:50 -0700

    modified:   my/mode-bindings.el

--- my/mode-bindings.el
+++ my/mode-bindings.el
@@ -88,8 +88,9 @@
             (keymap-set calc-edit-mode-map "M-5" (kmacro "^ 5"))
             (keymap-set calc-edit-mode-map "M-6" (kmacro "^ 6"))
             (keymap-set calc-edit-mode-map "M-q" (kmacro "s q r t ( ) C-b"))
-            (keymap-set calc-edit-mode-map "\\" 'my/calc-edit-square-dwim)
-            (keymap-set calc-edit-mode-map "|" 'my/calc-edit-sqrt-dwim)))
+            ;; (keymap-set calc-edit-mode-map "\"" (my/cmd (insert "'")))
+            (keymap-set calc-edit-mode-map "'" 'my/calc-edit-square-dwim)
+            (keymap-set calc-edit-mode-map "\\" 'my/calc-edit-sqrt-dwim)))
 
 (eval-after-load 'calendar
   '(progn
