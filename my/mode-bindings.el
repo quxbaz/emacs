f22bea138a026d2070ea4ca21169b2d3150b6cdf
David Yeung <quxbaz@gmail.com>
2025-11-20 12:40:15 -0700

    modified:   my/mode-bindings.el

--- my/mode-bindings.el
+++ my/mode-bindings.el
@@ -29,7 +29,6 @@
          (:map calc-mode-map ("S-<return>" . calc-enter))
          (:map calc-mode-map ("M-=" . calc-evaluate))
          (:map calc-mode-map ("=" . my/calc-evaluate))
-         (:map calc-mode-map ("C-c C-c" . my/calc-evaluate))
          (:map calc-mode-map ("o" . calc-inv))
          (:map calc-mode-map ("\\" . calc-sqrt))
          (:map calc-mode-map ("W" . my/calc-square))
