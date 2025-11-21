a7f85f995ec28c69d95ccc79986ad8f0a0997ff9
David Yeung <quxbaz@gmail.com>
2025-11-21 13:02:10 -0700

    modified:   my/mode-bindings.el

--- my/mode-bindings.el
+++ my/mode-bindings.el
@@ -45,6 +45,7 @@
          (:map calc-mode-map ("a s" . calc-simplify-extended))
          ;; Solving
          (:map calc-mode-map ("i" . calc-solve-for))
+         (:map calc-mode-map ("a l" . calc-poly-roots))
          (:map calc-mode-map ("M-." . calc-remove-equal))
          (:map calc-mode-map ("a m" . calc-map-equation))
          (:map calc-mode-map ("a o" . calc-collect))
