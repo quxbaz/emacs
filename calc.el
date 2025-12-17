bc6081dffa577d98b48eaaa0e4bd1db488dec293
David Yeung <quxbaz@gmail.com>
2025-12-17 12:17:32 -0700

    modified:   calc.el

--- calc.el
+++ calc.el
@@ -1,7 +1,7 @@
 
 ;;; Mode settings stored by Calc on Tue Oct 14 11:25:47 2025
 ;; (setq calc-context-sensitive-enter t)
-(setq calc-show-selections nil)
+(setq calc-show-selections t)
 (setq calc-display-trail nil)
 (setq calc-symbolic-mode t)
 (setq calc-prefer-frac t)
