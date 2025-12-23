a9d5c4ad8dbb1515192a83d0a60a0bb65120b0e5
David Yeung <quxbaz@gmail.com>
2025-12-23 14:28:59 -0700

    modified:   my/calc/lib.el

--- my/calc/lib.el
+++ my/calc/lib.el
@@ -5,11 +5,6 @@
 
 (defun my/calc-at-stack-bottom-p ()
   "Returns t if point is at the stack bottom or beyond it."
-  (save-excursion
-    (condition-case nil
-        (progn (next-line 2) (previous-line))
-      (error nil))
-    (let ((line (string-trim (substring-no-properties (thing-at-point 'line)))))
-      (string= line "."))))
+  (<= (calc-locate-cursor-element (point)) 1))
 
 (provide 'my/calc/lib)
