30d2195a96f88abfcf58d04d11e0c2975668c92b
David Yeung <quxbaz@gmail.com>
2025-12-24 14:21:08 -0700

    [calc] Move functions to lib.

--- my/calc/lib.el
+++ my/calc/lib.el
@@ -7,4 +7,15 @@
   "Returns t if point is at the stack bottom or beyond it."
   (<= (calc-locate-cursor-element (point)) 1))
 
+(defun my/calc-no-selection-p ()
+  "Returns t if there are no active selections."
+  (not (my/calc-no-selection-p)))
+
+(defun my/calc-active-selection-p ()
+  "Returns t if there are any active selections."
+  (and calc-use-selections
+       ;; If any stack element has a non-nil value at (nth 2 elt), then
+       ;; selection is active.
+       (seq-some (lambda (elt) (nth 2 elt)) calc-stack)))
+
 (provide 'my/calc/lib)
--- my/calc/selection.el
+++ my/calc/selection.el
@@ -3,17 +3,6 @@
 ;; Selection functions
 
 
-(defun my/calc-no-selection-p ()
-  "Returns t if there are no active selections."
-  (not (my/calc-no-selection-p)))
-
-(defun my/calc-active-selection-p ()
-  "Returns t if there are any active selections."
-  (and calc-use-selections
-       ;; If any stack element has a non-nil value at (nth 2 elt), then
-       ;; selection is active.
-       (seq-some (lambda (elt) (nth 2 elt)) calc-stack)))
-
 (defun my/calc-clear-selections ()
   "Like calc-clear-selections, but retains point position."
   (interactive)
