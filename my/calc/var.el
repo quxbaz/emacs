695431bf80b1b11e95ebebfeed4468f973370139
David Yeung <quxbaz@gmail.com>
2025-12-24 15:18:22 -0700

    new file:   my/calc/var.el

---
+++ my/calc/var.el
@@ -0,0 +1,12 @@
+;; -*- lexical-binding: t; -*-
+;;
+;; Variable declarations
+
+
+(defvar-local my/calc-edit-saved-point nil
+  "Variable used to restore point in some cases after exiting edit-mode.")
+
+(defvar-local my/calc-saved-stack nil
+  "Used in my/calc-save-stack and my/calc-restore-stack.")
+
+(provide 'my/calc/var)
