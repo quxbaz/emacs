adb0989b8a05f114dda0d54a852536c7d23482f8
David Yeung <quxbaz@gmail.com>
2025-12-31 14:04:54 -0700

    modified:   scratch/factor-by.el

--- scratch/factor-by.el
+++ scratch/factor-by.el
@@ -9,8 +9,12 @@
         (replace-expr (or (nth 1 bindings) (gensym)))
         (sel-is-active (or (nth 2 bindings) (gensym)))
         (m (or (car (alist-get 'm options)) 1))
-        (prefix (or (car (alist-get 'prefix options)) "")))
-    `(let ((,sel-is-active (my/calc-active-selection-p)))
+        (prefix (or (car (alist-get 'prefix options)) ""))
+        ;; TODO: Fix this. Default to `t` when not assigned.
+        ;; (keep-point (or (car (alist-get 'keep-point options)) t))
+        (keep-point nil))
+    `(let ((,sel-is-active (my/calc-active-selection-p))
+           (saved-point (point)))
        (cond (,sel-is-active
               (let* ((m (my/calc-active-entry-m-dwim))
                      (entry (nth m calc-stack))
@@ -23,21 +27,20 @@
               (let ((,expr (calc-top-n ,m)))
                 (cl-flet ((,replace-expr (new-expr)
                             (calc-pop-push-record-list 1 ,prefix new-expr ,m)))
-                  ,@body)))))))
+                  ,@body))))
+       (if ,keep-point
+           (setf (point) saved-point)))))
 
 (defun my/calc-factor-by ()
   (interactive)
-  (my/calc-apply-sel-or-top (expr replace-expr sel-is-active) ((m 2) (prefix "fctr"))
+  (my/calc-apply-sel-or-top (expr replace-expr sel-is-active) ((prefix "fctr") (m 2) (keep-point t))
     (my/calc-dont-simplify
-     (let* ((saved-point (point))
-            (factor (calc-top-n 1))
+     (let* ((factor (calc-top-n 1))
             (divided (math-simplify (calcFunc-nrat (calcFunc-expand (calcFunc-div expr factor)))))
             (product (calcFunc-mul factor divided)))
        (calc-wrapper
         (replace-expr product)
-        (calc-pop-stack 1))
-       (if sel-is-active
-           (setf (point) saved-point))))))
+        (calc-pop-stack 1))))))
 
 (my/calc-apply-sel-or-top (expr replace-expr) ((m 2) (prefix "fctr"))
   (my/calc-dont-simplify
