37418a042f3a6d1ff7f3b65bddbe250c6e00816f
David Yeung <quxbaz@gmail.com>
2025-12-31 13:22:56 -0700

    modified:   scratch/factory-by.el

--- scratch/factory-by.el
+++ scratch/factory-by.el
@@ -24,6 +24,27 @@
                                (calc-pop-push-record-list 1 ,prefix new-expr m)))))
        ,@body)))
 
+(defmacro my/calc-apply-sel-or-top (bindings options &rest body)
+  (declare (indent 2))
+  (let ((expr (or (nth 0 bindings) (gensym)))
+        (replace-expr (or (nth 1 bindings) (gensym)))
+        (sel-is-active (or (nth 2 bindings) (gensym)))
+        (m (or (car (alist-get 'm options)) 1))
+        (prefix (or (car (alist-get 'prefix options)) "")))
+    `(let ((,sel-is-active (my/calc-active-selection-p)))
+       (cond (,sel-is-active
+              (let* ((m (if (my/calc-active-selection-at-line-p)
+                            (calc-locate-cursor-element (point))
+                          (my/calc-first-active-entry-m))))
+                nil))
+             (t
+              (let* ((expr (calc-top-n ,m))
+                     (,replace-expr))
+                (fset ',replace-expr
+                      (lambda (new-expr)
+                        (calc-pop-push-record-list 1 ,prefix new-expr ,m)))
+                ,@body))))))
+
 (defun my/calc-factor-by ()
   (interactive)
   (my/calc-apply-sel-or-top (expr replace-expr) ((m 2) (prefix "fctr"))
@@ -33,5 +54,15 @@
             (product (calcFunc-mul factor divided)))
        (my/preserve-point
         (calc-wrapper
-         (calc-pop-stack 1)
-         (funcall replace-expr product)))))))
+         (replace-expr product)
+         (calc-pop-stack 1)))))))
+
+(my/calc-apply-sel-or-top (expr replace-expr) ((m 2) (prefix "fctr"))
+  (my/calc-dont-simplify
+   (let* ((factor (calc-top-n 1))
+          (divided (math-simplify (calcFunc-nrat (calcFunc-expand (calcFunc-div expr factor)))))
+          (product (calcFunc-mul factor divided)))
+     (my/preserve-point
+      (calc-wrapper
+       (calc-pop-stack 1)
+       (replace-expr product))))))
