45250c4215ce31c289bb588b3e8b7861a43b2d3d
David Yeung <quxbaz@gmail.com>
2026-01-01 14:01:54 -0700

    modified:   scratch/factor-by.el

--- scratch/factor-by.el
+++ scratch/factor-by.el
@@ -5,30 +5,30 @@
 ;; TODO: Add comments.
 (defmacro my/calc-apply-sel-or-top (bindings options &rest body)
   (declare (indent 2))
-  (let ((expr (or (nth 0 bindings) (gensym)))
-        (replace-expr (or (nth 1 bindings) (gensym)))
-        (sel-is-active (or (nth 2 bindings) (gensym)))
-        (m (or (car (alist-get 'm options)) 1))
-        (prefix (or (car (alist-get 'prefix options)) ""))
+  (let ((sym-expr (or (nth 0 bindings) (gensym)))
+        (sym-replace-expr (or (nth 1 bindings) (gensym)))
+        (sym-sel-is-active (or (nth 2 bindings) (gensym)))
+        (opt-m (or (car (alist-get 'm options)) 1))
+        (opt-prefix (or (car (alist-get 'prefix options)) ""))
         ;; TODO: Fix this. Default to `t` when not assigned.
         ;; (keep-point (or (car (alist-get 'keep-point options)) t))
-        (keep-point nil))
-    `(let ((,sel-is-active (my/calc-active-selection-p))
+        (opt-keep-point nil))
+    `(let ((,sym-sel-is-active (my/calc-active-selection-p))
            (saved-point (point)))
-       (cond (,sel-is-active
+       (cond (,sym-sel-is-active
               (let* ((m (my/calc-active-entry-m-dwim))
                      (entry (nth m calc-stack))
-                     (,expr (nth 2 entry)))
-                (cl-flet ((,replace-expr (new-expr)
-                            (let ((new-formula (calc-replace-sub-formula (car entry) ,expr new-expr)))
-                              (calc-pop-push-record-list 1 ,prefix new-formula m new-expr))))
+                     (,sym-expr (nth 2 entry)))
+                (cl-flet ((,sym-replace-expr (new-expr)
+                            (let ((new-formula (calc-replace-sub-formula (car entry) ,sym-expr new-expr)))
+                              (calc-pop-push-record-list 1 ,opt-prefix new-formula m new-expr))))
                   ,@body)))
              (t
-              (let ((,expr (calc-top-n ,m)))
-                (cl-flet ((,replace-expr (new-expr)
-                            (calc-pop-push-record-list 1 ,prefix new-expr ,m)))
+              (let ((,sym-expr (calc-top-n ,opt-m)))
+                (cl-flet ((,sym-replace-expr (new-expr)
+                            (calc-pop-push-record-list 1 ,opt-prefix new-expr ,opt-m)))
                   ,@body))))
-       (if ,keep-point
+       (if ,opt-keep-point
            (setf (point) saved-point)))))
 
 (defun my/calc-factor-by ()
