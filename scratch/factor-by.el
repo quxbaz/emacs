3034ba9ebe759b7de9dde5577a1e7645eb4cd321
David Yeung <quxbaz@gmail.com>
2026-01-01 14:14:42 -0700

    modified:   scratch/factor-by.el

--- scratch/factor-by.el
+++ scratch/factor-by.el
@@ -8,11 +8,9 @@
   (let ((sym-expr (or (nth 0 bindings) (gensym)))
         (sym-replace-expr (or (nth 1 bindings) (gensym)))
         (sym-sel-is-active (or (nth 2 bindings) (gensym)))
-        (opt-m (or (car (alist-get 'm options)) 1))
-        (opt-prefix (or (car (alist-get 'prefix options)) ""))
-        ;; TODO: Fix this. Default to `t` when not assigned.
-        ;; (keep-point (or (car (alist-get 'keep-point options)) t))
-        (opt-keep-point nil))
+        (opt-m (alist-get :m options 1))
+        (opt-prefix (alist-get :prefix options ""))
+        (opt-keep-point (alist-get :keep-point options t)))
     `(let ((,sym-sel-is-active (my/calc-active-selection-p))
            (saved-point (point)))
        (cond (,sym-sel-is-active
@@ -28,12 +26,13 @@
                 (cl-flet ((,sym-replace-expr (new-expr)
                             (calc-pop-push-record-list 1 ,opt-prefix new-expr ,opt-m)))
                   ,@body))))
-       (if ,opt-keep-point
-           (setf (point) saved-point)))))
+       (unless (eq ,opt-keep-point -1)
+         (setf (point) saved-point)))))
 
 (defun my/calc-factor-by ()
   (interactive)
-  (my/calc-apply-sel-or-top (expr replace-expr sel-is-active) ((prefix "fctr") (m 2) (keep-point t))
+  (my/calc-apply-sel-or-top (expr replace-expr sel-is-active)
+      ((:prefix . "fctr") (:m . 2) (:keep-point . t))
     (my/calc-dont-simplify
      (let* ((factor (calc-top-n 1))
             (divided (math-simplify (calcFunc-nrat (calcFunc-expand (calcFunc-div expr factor)))))
@@ -42,7 +41,7 @@
         (replace-expr product)
         (calc-pop-stack 1))))))
 
-(my/calc-apply-sel-or-top (expr replace-expr) ((m 2) (prefix "fctr"))
+(my/calc-apply-sel-or-top (expr replace-expr) ((:m . 2) (:prefix . "fctr"))
   (my/calc-dont-simplify
    (let* ((factor (calc-top-n 1))
           (divided (math-simplify (calcFunc-nrat (calcFunc-expand (calcFunc-div expr factor)))))
