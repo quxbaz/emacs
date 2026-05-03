51d1877126b1723b2aea58cd7d17a558ee27a73d
David Yeung <quxbaz@gmail.com>
2026-05-03 12:23:52 -0600

    Fix indent

--- my/calc/bindings.el
+++ my/calc/bindings.el
@@ -44,7 +44,7 @@
          (:map calc-mode-map ("C-k" . my/calc-kill))
          (:map calc-mode-map ("C-a" . my/calc-beginning-of-expression))
          (:map calc-mode-map ("M-m" . my/calc-beginning-of-expression))
-(:map calc-mode-map ("M-f" . my/calc-forward-noun))
+         (:map calc-mode-map ("M-f" . my/calc-forward-noun))
          (:map calc-mode-map ("M-b" . my/calc-backward-noun))
          (:map calc-mode-map ("M-w" . my/calc-kill-ring-save-dwim))
          (:map calc-mode-map ("DEL" . my/calc-pop))
