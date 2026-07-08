633f7f4b35a853f37015b44157b940f378db2431
David Yeung <quxbaz@gmail.com>
2026-07-08 15:51:46 -0600

    use maf instead of my/calc

--- init.el
+++ init.el
@@ -12,10 +12,10 @@
 
 ;; Local packages
 ;; Load maf package if it exists.
-;; (let ((maf-path (concat user-emacs-directory "site-lisp/maf")))
-;;   (when (file-exists-p (concat maf-path "/maf.el"))
-;;     (add-to-list 'load-path maf-path)
-;;     (require 'maf)))
+(let ((maf-path (concat user-emacs-directory "site-lisp/maf")))
+  (when (file-exists-p (concat maf-path "/maf.el"))
+    (add-to-list 'load-path maf-path)
+    (require 'maf)))
 
 ;; Load wire package if it exists.
 (let ((wire-path (concat user-emacs-directory "site-lisp/wire")))
@@ -66,8 +66,6 @@
 (load-file (concat user-emacs-directory "my/kmacros.el"))
 ;; Per-mode configuration (settings + hooks + bindings, one block per mode)
 (load-file (concat user-emacs-directory "my/modes.el"))
-;; Calc
-(load-file (concat user-emacs-directory "my/calc/index.el"))
 
 ;; Autoloads
 (autoload 'sql-lisp-mode (concat user-emacs-directory "my/lib-sql.el") "A mode for SQL interaction through evaluation of Emacs Lisp forms." t)
