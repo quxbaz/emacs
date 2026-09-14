de886a8fb24344b4b33af2957fb37a9b235fdadd
David Yeung <quxbaz@gmail.com>
2026-09-14 11:29:03 -0600

    modified:   my/bindings.el

--- my/bindings.el
+++ my/bindings.el
@@ -28,9 +28,9 @@
 
 ;; # Testing ergonomic bindings
 ;; (keymap-set my/override-map "C-n" (my/cmd (user-error "Use C-j")))
-;; (keymap-set my/override-map "C-p" (my/cmd (user-error "Use C-k")))
+(keymap-set my/override-map "C-p" (my/cmd (user-error "Use C-k")))
 (keymap-set my/override-map "C-n" 'scroll-up-command)
-(keymap-set my/override-map "C-M-n" 'scroll-down-command)
+(keymap-set my/override-map "M-n" 'scroll-down-command)
 (keymap-set my/override-map "C-j" (my/delegate-key "C-n"))
 (keymap-set my/override-map "C-k" (my/delegate-key "C-p"))
 (keymap-set my/override-map "M-k" (my/delegate-key "C-k"))
